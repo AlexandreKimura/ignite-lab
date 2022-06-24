16395ca07a41e6ce8cd4242663d183dfc71a316c
Alexandre Fukano <br-amrcld-alexandrefukano@kpmgamr.onmicrosoft.com>
2022-06-24 17:33:53 -0300

    Add phosfor

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,13 @@
+import { Lesson } from "./Lesson";
+
 export function Sidebar() {
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl">Vídeos</span>
+
+      <div className="flex flex-col gap-8">
+        <Lesson title="Aula 1" slug="aula-01" availableAt={new Date()} type='live'/>
+      </div>
     </aside>
   );
 }
